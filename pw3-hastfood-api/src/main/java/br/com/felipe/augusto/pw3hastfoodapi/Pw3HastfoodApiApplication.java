ce35221821eb4c7adba9beb6ac5f6563d91a6ba5
Felipe <felipeaug@test.com>
2022-03-30 13:31:13 -0300

    Criacao do projeto

---
+++ pw3-hastfood-api/src/main/java/br/com/felipe/augusto/pw3hastfoodapi/Pw3HastfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.augusto.pw3hastfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3HastfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3HastfoodApiApplication.class, args);
+	}
+
+}
---
+++ pw3-hastfood-api/src/test/java/br/com/felipe/augusto/pw3hastfoodapi/Pw3HastfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipe.augusto.pw3hastfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HastfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
